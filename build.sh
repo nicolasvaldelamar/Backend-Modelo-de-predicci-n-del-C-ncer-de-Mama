04113be3fc90ed66de1ae643c60585227c815aac
Nicolas Valdelamar <nvaldelamar@inovabiz.com>
2024-11-25 12:10:11 -0500

    test render 2

--- build.sh
+++ build.sh
@@ -4,11 +4,7 @@ set -o errexit
 
 # Instalar dependencias de Python
 python -m pip install --upgrade pip
-pip install setuptools wheel
 pip install -r requirements.txt
 
 # Instalar dependencias de Node
 npm install
-
-# Ejecutar el script de Python para generar el modelo
-python model.py 
